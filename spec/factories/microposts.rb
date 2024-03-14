20c7116fd72dea3007772702f368126269f4a511
Sawata <salt.mail0324@gmail.com>
2024-03-15 08:13:19 +0900

    追記/修正: [users.rb][microposts.rb][microposts_spec.rb]

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
-    factory :content do
-      # 英数字のランダムな文字列を生成する（100文字）
-      content { Faker::Lorem.characters(number: 100) }
-      # 上記の記述によりbuild(:content)でテストデータを生成できる
+    factory :micropost do
+      user
+      content { Faker::Lorem.paragraph_by_chars(number: 100) }
     end
   end
--- spec/models/microposts_spec.rb
+++ spec/models/microposts_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-    # letは遅延評価といい定義した定数が初めて出てきた時に評価される
-    let(:micropost) { build(:micropost) }
-    it "のコンテントが有効の時" do
+  let(:user) { create(:user) }
+  let(:micropost) { create(:micropost) }
+  
+  describe 'バリデーション' do
+    specify 'のuser_id, contentがある場合有効' do
+      expect(micropost).to be_valid
+    end 
+
+    it "のコンテントが有効" do
       expect(micropost).to be_valid
     end
-    it "のコンテントが空で無効の時" do
+
+    it "のコンテントが空の時は無効" do
       micropost.content = ''
       expect(micropost).to_not be_valid
     end
-    it "のコンテントが140字を超えて無効の時" do
+
+    it "のコンテントが140字を超えた時は無効" do
       micropost.content = Faker::Lorem.characters(number: 141)
       expect(micropost).to_not be_valid
     end
   end
+end
