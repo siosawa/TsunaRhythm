ef011ac40140dcd531858333faf765a14425da01
Sawata <salt.mail0324@gmail.com>
2024-03-15 09:16:13 +0900

    修正: ポスト投稿時のエラーメッセージを日本語に変更[micropost.rb]

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,7 +7,7 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
-                                      message: "must be a valid image format" },
+                                      message: "有効な画像形式である必要があります" },
                       size: { less_than: 5.megabytes,
-                              message:   "should be less than 5MB" }
+                              message:   "5MB未満である必要があります" }
 end
