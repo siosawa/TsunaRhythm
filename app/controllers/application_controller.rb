395440125c2b488e0e574fe2ed57e5a4ed6de551
Sawata <salt.mail0324@gmail.com>
2024-03-20 11:31:56 +0900

    correction: flash ja[application_controller.rb]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = "ログインして下さい"
         redirect_to login_url, status: :see_other
       end
     end
