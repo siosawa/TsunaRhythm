59a89ed915a7310316f61434f87d3a1fb70423df
Sawata <salt.mail0324@gmail.com>
2024-03-21 19:33:48 +0900

    修正: テストを修正[relationships_spec.rb]

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -1,31 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe RelationshipsController, type: :request do
-  let(:user) { create(:user) }
-  let(:other_user) { create(:user) }
-  let(:target_user) { create(:user) }
-  let(:unregistered_user) { build(:user) }
+  describe 'ログイン確認テスト' do
+    let(:user) { create(:user) }
+    let(:unregistered_user) { build(:user) } 
 
-describe 'POST #create' do
-  context '存在するユーザー' do
-    specify 'ログインが成功するとルートパスにリダイレクトする' do
-      session_params = { email: user.email, password: user.password, remember_me: 0 }
-      post login_path, params: { session: session_params }
-      expect(response).to redirect_to(root_url) 
+    context '存在するユーザーは' do
+      specify 'ログインが成功しルートパスにリダイレクトする' do
+        session_params = { email: user.email, password: user.password, remember_me: 0 }
+        post login_path, params: { session: session_params }
+        expect(response).to redirect_to(root_url) 
+      end
+    end
+
+    context '存在しないユーザーは' do
+      specify 'ログインが失敗しnewテンプレートがレンダーされる' do
+        session_params = { email: unregistered_user.email, password: unregistered_user.password, remember_me: 0 }
+        post login_path, params: { session: session_params }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response).to render_template('new')
+      end
     end
   end
-
-  context '存在しないユーザー' do
-    specify 'ログインが失敗する' do
-      session_params = { email: unregistered_user.email, password: unregistered_user.password, remember_me: 0 }
-      post login_path, params: { session: session_params }
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response).to render_template('new')
-    end
-  end
-end
 
   describe 'POST #create' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) } 
+
     context 'ログインしていない状態で' do
       it 'フォローするとログイン画面にリダイレクトする' do
         post relationships_path, params: { followed_id: other_user.id }
@@ -39,33 +40,44 @@ end
         post login_path, params: { session: session_params }
       end
 
-      it "ユーザーをフォローするとRelationshipの数が1増える" do
+      it "フォローするとRelationshipの数が1増える" do
         expect {
         post relationships_path, params: { followed_id: other_user.id }
         }.to change(Relationship, :count).by(1)
       end
+
+      # すでにフォローしているユーザーをフォローするとエラーになる
     end
+    
   end
 
   describe 'DELETE #destroy' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+    
     context 'ログインしていない状態で' do
-      it 'フォロー解除しようとするとログイン画面にリダイレクトされる' do
-        relationship = Relationship.new(follower_id: user.id, followed_id: target_user.id)
-        relationship.save
+      before do
+        @relationship = user.active_relationships.create(followed_id: other_user.id)
+      end
+
+      it 'フォロー解除するとRelationshipの数が変わらない' do
         expect {
-          delete relationship_path(relationship)
-        }.not_to change(Relationship, :count)
+          delete relationship_path(@relationship)
+        }.to_not change(Relationship, :count)
+        expect(response).to redirect_to(login_path)  
+      end
+      
+      it 'フォロー解除すると失敗し、ログイン画面にリダイレクトする' do
+        delete relationship_path(@relationship)
         expect(response).to redirect_to(login_path)
       end
     end
 
-
-    context 'ログインしている状態で' do
+    context 'ログインしている かつ other_userをフォローしている状態で' do
       before do
         session_params = { email: user.email, password: user.password, remember_me: 0 }
         post login_path, params: { session: session_params }
-        post relationships_path, params: { followed_id: target_user.id }
-        @relationship = user.active_relationships.create(followed_id: target_user.id)
+        @relationship = user.active_relationships.create(followed_id: other_user.id)
       end
 
       it 'フォロー解除をするとRelationshipの数が1減少する' do
@@ -73,8 +85,6 @@ end
           delete relationship_path(@relationship)
         }.to change(Relationship, :count).by(-1)
       end
-
-      #フォローしていないユーザーのフォローを解除しようとすると操作失敗の情報を返す
     end  
   end
 end
