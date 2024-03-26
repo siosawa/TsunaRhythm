311a44672589c82b0b0154734e1eed49b51ce96a
Sawata <salt.mail0324@gmail.com>
2024-03-26 18:52:54 +0900

    追加:Rails/InverseOf:問題を解消[models/user.rb][models/relationship.rb]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
-                                  dependent: :destroy
+                                  dependent: :destroy,
+                                  inverse_of: :follower
+
   has_many :passive_relationships, class_name: 'Relationship',
                                    foreign_key: 'followed_id',
-                                   dependent: :destroy
+                                   dependent: :destroy,
+                                   inverse_of: :followed
+
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
